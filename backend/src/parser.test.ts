708f3483d9bd40c6795e6b9eeccbdbc869525323
Joni Nevalainen <joninevalainen@gmail.com>
2021-09-11 20:37:35 +0300

    Add initial parser for simple field

--- backend/src/parser.test.ts
+++ backend/src/parser.test.ts
@@ -1,7 +1,49 @@
-import { add } from "./parser";
+import { parseName, parseParagraph, parseSimpleValue } from "./parser";
 
-describe("add", () => {
-  it("1 + 2", () => {
-    expect(add(1, 2)).toBe(3);
+describe("parseName", () => {
+  it("parses correct attribute name", () => {
+    const [name, _] = parseName("Package: libws-commons-util-java");
+    expect(name).toBe("Package");
+  });
+});
+
+describe("parseSimpleValue", () => {
+  it("simple field", () => {
+    const [value, _] = parseSimpleValue(" libws-commons-util-java\n");
+    expect(value).toBe("libws-commons-util-java");
+  });
+});
+
+describe("parseParagraph", () => {
+  it("smoke test", () => {
+    const paragraph = `
+Package: libws-commons-util-java
+Status: install ok installed
+Priority: optional
+Section: java
+Installed-Size: 101
+Maintainer: Ubuntu Developers <ubuntu-devel-discuss@lists.ubuntu.com>
+Architecture: all
+Version: 1.0.1-7
+Original-Maintainer: Debian Java Maintainers <pkg-java-maintainers@lists.alioth.debian.org>
+Homepage: http://ws.apache.org/commons/util/
+`.trim();
+
+    const result = parseParagraph(paragraph);
+
+    expect(result["Package"]).toBe("libws-commons-util-java");
+    expect(result["Status"]).toBe("install ok installed");
+    expect(result["Priority"]).toBe("optional");
+    expect(result["Section"]).toBe("java");
+    expect(result["Installed-Size"]).toBe("101");
+    expect(result["Maintainer"]).toBe(
+      "Ubuntu Developers <ubuntu-devel-discuss@lists.ubuntu.com>"
+    );
+    expect(result["Architecture"]).toBe("all");
+    expect(result["Version"]).toBe("1.0.1-7");
+    expect(result["Original-Maintainer"]).toBe(
+      "Debian Java Maintainers <pkg-java-maintainers@lists.alioth.debian.org>"
+    );
+    expect(result["Homepage"]).toBe("http://ws.apache.org/commons/util/");
   });
 });
--- backend/src/parser.ts
+++ backend/src/parser.ts
@@ -1,1 +1,49 @@
-export const add = (a: number, b: number) => a + b
+/**
+ * Functions related to parsing the packages file.
+ *
+ * Each parser has common interface. It will take the unconsumed paragraph as input,
+ * try to parse desired value from the beginning of the input and return the parsed
+ * value and the leftover input that can be fed to the next parser.
+ */
+
+type ParseResult = [string, string];
+
+export const parseName = (input: string): ParseResult => {
+  const i = input.indexOf(":");
+  const name = input.substring(0, i);
+  const rest = input.substring(i + 1);
+
+  // TODO: validate that name conforms the specification
+  return [name, rest];
+};
+
+export const parseSimpleValue = (input: string): ParseResult => {
+  const i = input.indexOf("\n");
+
+  // Last line of paragraph will not have new line. Use the entire line as value.
+  if (i === -1) {
+    return [input.trim(), ""];
+  }
+
+  const value = input.substring(0, i).trim();
+  const rest = input.substring(i + 1);
+  return [value, rest];
+};
+
+export const parseParagraph = (paragraph: string): Record<string, string> => {
+  let output: Record<string, string> = {};
+  let value = paragraph;
+
+  while (value.trim().length > 0) {
+    const [name, rest] = parseName(value);
+    const [fieldValue, rest2] = parseSimpleValue(rest);
+    output[name] = fieldValue;
+    value = rest2;
+  }
+
+  // TODO:
+  // - check for duplicate keys
+  // - handle comments
+
+  return output;
+};
